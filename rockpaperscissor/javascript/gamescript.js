97ea7ee45e0c48d453dc513e8ab0575a0cb022e2
Joshuasle13 <joshsle22@gmail.com>
2024-08-05 12:01:08 -0400

    display the choices that you made, counter and a button to restart the page

--- rockpaperscissor/javascript/gamescript.js
+++ rockpaperscissor/javascript/gamescript.js
@@ -3,11 +3,18 @@
 const   rock    =   document.getElementById('btn1');
 const   paper   =   document.getElementById('btn2');
 const   scissor =   document.getElementById('btn3');
-let selection   =   document.getElementById('selected')
-let computer    =   document.getElementById('computer')
+let selection   =   document.getElementById('selected');
+let computer    =   document.getElementById('computer');
+let reset       =   document.getElementById('reset');
+let hScore      =   document.getElementById('humScore');
+let cScore      =   document.getElementById('comScore');
 
-
-
+function updateScore (){
+hScore.innerHTML    =   humanScore;
+cScore.innerHTML    =   computerScore;
+selection.removeChild()
+computer.removeChild()
+}
 
 
 function getComputerChoice  (){
@@ -21,6 +28,10 @@ function getComputerChoice  (){
     }
     }
      
+  
+
+
+
 
      var    humanScore  =   0;
      var    computerScore   =   0;
@@ -31,70 +42,92 @@ function evento(){
 
  
     rock.addEventListener('click',()=>{
-        let cchoice = getComputerChoice();
-        num =   'Rock';
-     
         
-       
+        let cchoice = getComputerChoice();
+        let humanchoice   =  document.createElement('span');
+        humanchoice.textContent =   ' - Rock -';
+        selection.appendChild(humanchoice)
+        num =   'Rock';
+        let dcoputer    =   document.createElement('span');
+        dcoputer.textContent    =   ` - ${cchoice} -`
+        computer.appendChild(dcoputer)
+
+        
       
         if( num === cchoice){
             alert ('draw')
         }else if(num !== cchoice && cchoice=== 'Scissor'){
-            alert(`${num} vs ${cchoice}, you win`),   humanScore++;
+            alert('you win'),   humanScore++;
         }else if(num !== cchoice && cchoice=== 'Paper'){
-            alert(`${num} vs ${cchoice}, you lose`),  computerScore++ ;}
+            alert('you lose'),  computerScore++ ;}
             console.log(humanScore,computerScore);
             if(humanScore   === 5){
                 alert('you won'),   humanScore    =   0,    computerScore  =   0;
             }else if(computerScore  ===  5){
                 alert('you lost'),  humanScore    =   0, computerScore   =   0;
-            }
+            } updateScore();
       })
+
+  
   
     paper.addEventListener('click',()=>{
-        let cchoice = getComputerChoice();
+        let cchoice = getComputerChoice()
+        let humanchoice   =  document.createElement('span');
+        humanchoice.textContent=   ' - Paper -';
+        selection.appendChild(humanchoice);
         num =   'Paper';
-       
+        let dcoputer    =   document.createElement('span');
+        dcoputer.textContent    =   ` - ${cchoice} -`;
+        computer.appendChild(dcoputer);
         
        
 
         if( num === cchoice){
             alert ('draw')
         }else if(num !== cchoice && cchoice=== 'Scissor'){
-            alert(`${num} vs ${cchoice}, you lose`),  computerScore++;
+            alert('you lose'),  computerScore++;
         }else if(num !== cchoice && cchoice=== 'Rock'){
-            alert(`${num} vs ${cchoice}, you win`),   humanScore++; }
+            alert('you win'),   humanScore++; }
             console.log(humanScore,computerScore);
             if(humanScore   === 5){
                 alert('you won'),   humanScore    =   0,    computerScore  =   0;
             }else if(computerScore  ===  5){
                 alert('you lost'),  humanScore    =   0, computerScore   =   0;
-            };
+            } updateScore();
             
         })
   
     scissor.addEventListener('click',()=>{
-        let cchoice = getComputerChoice();
+        let cchoice = getComputerChoice()
+        let humanchoice   =  document.createElement('span');
+        humanchoice.textContent=   ' - Scissor -';
+        selection.appendChild(humanchoice);
         num =   'Scissor';
-      
+        let dcoputer    =   document.createElement('span');
+        dcoputer.textContent    =   ` - ${cchoice} -`;
+        computer.appendChild(dcoputer);
     
 
         if( num === cchoice){
             alert ('draw')
         }else if(num !== cchoice && cchoice=== 'Rock'){
-            alert(`${num} vs ${cchoice}, you lose`),  computerScore++;
+            alert('you lose'),  computerScore++;
         }else if(num !== cchoice && cchoice=== 'Paper'){
-            alert(`${num} vs ${cchoice}, you win`),   humanScore++;}
+            alert('you win'),   humanScore++;}
             console.log(humanScore,computerScore);
             if(humanScore   === 5){
                 alert('you won'),   humanScore    =   0,    computerScore  =   0;
             }else if(computerScore  ===  5){
                 alert('you lost'),  humanScore    =   0, computerScore   =   0;
-            }
+            } updateScore();
+
+            
         })
         
        
-      
+        reset.addEventListener('click', ()=>{
+            location.reload()
+        })
         
     }
 
